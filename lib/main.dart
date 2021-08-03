592baaa5044f2056831eb3f70aaaf5b41780a554
evodesenvolvimentos <evotreinamentos@gmail.com>
2021-08-02 22:16:00 -0300

    inseri device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vscode3/app/routes/app_pages.dart';
 import 'package:get/get.dart';
@@ -6,7 +8,7 @@ import 'app/routes/app_routes.dart';
 
 void main() {
   runApp(
-      GetMaterialApp(
+/*      GetMaterialApp(
         title: 'Teste',
         debugShowCheckedModeBanner: false,
         getPages: AppPages.routes,
@@ -14,5 +16,22 @@ void main() {
         //initialRoute: Routes.HOME2,  // rota com resposividade  funcionando
         themeMode: ThemeMode.system,
         locale: Locale('pt','BR'),
-      )
+      )*/
+
+      DevicePreview(
+        builder: (context)=>
+        GetMaterialApp(
+          title: 'Teste',
+          debugShowCheckedModeBanner: false,
+          getPages: AppPages.routes,
+          //initialRoute: Routes.HOME,
+          initialRoute: Routes.HOME2,  // rota com resposividade  funcionando
+          themeMode: ThemeMode.system,
+          locale: Locale('pt','BR'),),
+        
+        enabled: !kReleaseMode,
+      ),
+      
+      
+
   );}
